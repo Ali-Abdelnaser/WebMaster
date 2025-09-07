75737e7f44fc53edd1b72d300fa3b3d9cc8da432
Ali-Abdelnaser <alinaserhema60@gmail.com>
2025-09-07 05:58:17 +0300

    Update New toggle

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const header = document.querySelector("header");
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.querySelector(".nav-links");
+const overlay = document.getElementById("drawer-overlay");
 const logo = document.getElementById("logo");
-
+const carousel = document.getElementById("eventsCarousel");
+const dots = document.querySelectorAll(".dot");
+let currentIndex = 0;
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     header.classList.add("scrolled");
@@ -15,12 +18,17 @@ window.addEventListener("scroll", () => {
 
 toggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+  overlay.classList.toggle("show");
+  toggle.classList.toggle("active");
 });
 
+overlay.addEventListener("click", () => {
+  navLinks.classList.remove("show");
+  overlay.classList.remove("show");
+  toggle.classList.remove("active");
+});
 // Events
-const carousel = document.getElementById("eventsCarousel");
-const dots = document.querySelectorAll(".dot");
-let currentIndex = 0;
+
 
 function updateCarousel(index) {
   const cardWidth = carousel.offsetWidth;
