15d155670d222d0df372f154179a5a33e29b2257
Ali-Abdelnaser <alinaserhema60@gmail.com>
2025-09-16 13:45:44 +0300

    add updateeee

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // زود الحد لـ 1MB بدل 500KB
-  },
+    chunkSizeWarningLimit: 1000, // زود الحد من 500KB لـ 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'three', 'framer-motion'] // قسم المكتبات الكبيرة
+        }
+      }
+    }
+  }
 })
